049f1793f2390dfb2c9789bea1dcfce30ff10284
filha243 <filha243@student.liu.se>
2024-06-18 14:47:39 +0200

    test

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.mjs
 
+import { eslint } from 'next';
+
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  // other Next.js configuration options can go here
 };
 
 export default nextConfig;
