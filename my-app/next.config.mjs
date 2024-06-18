07e1ce2f9671c076adfcb0e99c97b808ed39c632
filha243 <filha243@student.liu.se>
2024-06-18 14:48:55 +0200

    Update next.config.mjs

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,14 +1,13 @@
 // next.config.mjs
 
-import { eslint } from 'next';
-
 const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  // other Next.js configuration options can go here
-};
-
-export default nextConfig;
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+    // other Next.js configuration options
+  };
+  
+  export default nextConfig;
+  
